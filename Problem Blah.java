a4d05a588a4a93b7e302be31a452400e219811e5
iamtuhinchakma <122518680+iamtuhinchakma@users.noreply.github.com>
2023-07-18 23:26:36 +0600

    Update Problem Blah.java

--- Problem Blah.java
+++ Problem Blah.java
@@ -1,15 +1,1 @@
-public class Counter {
-    private int value;
-    public Counter(){
-        value = 0;
-    }
-    public int getValue(){
-        return value;
-    }
-    public void click(){
-        value = value + 1;
-    }
-    public void reset(){
-        value = 0;
-    }
-}
+
